0666b33521c90d26427815b3d9793910817e1417
HaiMinhDoan <haiminh17052003@gmail.com>
2025-04-03 14:45:46 +0700

    a

--- slm_api/app/routers/users.py
+++ slm_api/app/routers/users.py
@@ -96,7 +96,7 @@ def get_user(id: int, db: Session = Depends(get_db)):
         #Sắp xếp combo_dict["pre_quote_merchandises"] theo merchandise.template.id
         combo_dict["pre_quote_merchandises"] = sorted(
             combo_dict["pre_quote_merchandises"],
-            key=lambda x: x["merchandise"]["template"]["id"]
+            key=lambda x: x["pre_quote_merchandise"]["merchandise"]["template"]["id"]
         )
     user_dict["contracts"] = combos_dict
     return user_dict
