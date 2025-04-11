7f45f3d236c6c72f90333fef99a1ccdff5fb7cb5
HaiMinhDoan <haiminh17052003@gmail.com>
2025-04-11 16:58:34 +0700

    a

--- slm_api/app/routers/file_api.py
+++ slm_api/app/routers/file_api.py
@@ -128,7 +128,7 @@ def generate_pre_quote_detail_pdf(pre_quote_id:int,request: Request, db: Session
         'no-outline': None,
         'disable-smart-shrinking': '',
         'header-html': os.path.abspath("app/templates/header_rendered.html"),
-        'footer-html': os.path.abspath("app/templates/header_rendered.html"),
+        'footer-html': os.path.abspath("app/templates/footer_rendered.html"),
         'header-spacing': '5',   # spacing giữa header và nội dung (px)
         'footer-spacing': '5',
         'print-media-type': '',  # <- Cho phép xử lý CSS in ấn (media="print")
