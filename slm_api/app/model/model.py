f2cd3aa60a1923b01a8291828bc172e3109f33c1
HaiMinhDoan <haiminh17052003@gmail.com>
2025-04-16 12:54:03 +0700

    a

--- slm_api/app/model/model.py
+++ slm_api/app/model/model.py
@@ -11,7 +11,7 @@ POSTGRES_HOST = "localhost"  # Hoặc địa chỉ server PostgreSQL
 POSTGRES_PORT = "5432"  # Cổng mặc định của PostgreSQL
 
 # DATABASE_URL = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}"
-DATABASE_URL = "postgresql://postgres:0146424Minh@i0s0ckccckk4o4k804cc8og8:5432/slm_app"
+DATABASE_URL = "postgres://postgres:0146424Minh@212.85.25.175:5432/slm_app"
 
 # Tạo engine kết nối
 engine = create_engine(DATABASE_URL, echo=True)
